21cd3e41bf4789786b0806b1e530785d3a49aa6f
ogaliz <oscargalvezr@live.com>
2018-01-13 01:45:15 +0100

    Primer proyecto con hola mundo desde Mint

---
+++ practicas/src/ejerciciosArea51/ProgramacionEstructurada.java
@@ -0,0 +1,30 @@
+package ejerciciosArea51;
+
+class Metodos{
+	
+	public static void saludo() {
+		
+		System.out.println("Hola mundo");
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+public class ProgramacionEstructurada {
+
+	public static void main(String[] args) {
+		
+		Metodos.saludo();
+		
+		
+		
+		
+	}//cierre del main
+
+}//cierre del class
